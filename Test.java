bd7ab36871bc9e492406ec324da438defd8a66fb
Abdullah <263678@usn.no>
2025-01-05 21:22:26 +0100

    Endring av java-fil bare med et eget gren tix-2

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class test{
     public static void main(String[] args){
         System.out.println("Test av java æææ"); 
         System.out.println("Nok en utvidelse av denne slitne teksten.");
+        System.out.println("Endring for ticket-2"); 
     }
 }
