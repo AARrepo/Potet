f462a7c65cf3f1fcb39f7f3a06487b58736d667e
Abdullah <263678@usn.no>
2025-01-08 14:53:25 +0100

    #6 La til en kommentar til

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ class test{
     public static void main(String[] args){
         System.out.println("Nok en utvidelse av denne slitne teksten.");
         System.out.println("Her har vi virkelig gjort endringer"); 
+        System.out.println("Enda mer endringer"); 
          
     }
 }
